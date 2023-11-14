99bacfce667286cdba22fa8c80ed3f4e7ba51014
Uziel Ludueña <uzielluduena@MacBook-Pro-de-Uziel.local>
2023-11-14 11:50:11 -0300

    chore: docs

--- src/deploys.ts
+++ src/deploys.ts
@@ -1,4 +1,4 @@
-import { Address, Chain, zeroAddress } from "viem";
+import { Address, Chain, Hash, zeroAddress } from "viem";
 import { goerli } from "viem/chains";
 
 const ANVIL_CHAIN_ID = 31337;
@@ -24,7 +24,7 @@ interface Currencies {
   USDC_ADDRESS: Address;
 }
 
-export const MSL_V5_TX_HASH = '0xb6dfcbc1661d0c0bced9591d06e964f97d41a35984704ffe61f8e062e43919c8';
+export const MSL_V5_TX_HASH = '0xb6dfcbc1661d0c0bced9591d06e964f97d41a35984704ffe61f8e062e43919c8' as Hash;
 
 export const getContracts = (chain: Pick<Chain, "id">): Contracts => {
   if (chain?.id === ANVIL_CHAIN_ID) {
