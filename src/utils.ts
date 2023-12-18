e6f2ea63e4244a8065b8a84da7e53e478f255a83
Uziel Ludueña <uzielluduena@MacBook-Pro-de-Uziel.local>
2023-12-18 11:33:53 -0300

    feat: added 2.1 version to domain

--- src/utils.ts
+++ src/utils.ts
@@ -12,8 +12,24 @@ export const areSameAddress = (
 };
 
 export const getDomain = (chainId: number, verifyingContract: Address) => {
-  const contracts = getContracts({ id: chainId });
-  const version = areSameAddress(verifyingContract, contracts.MultiSourceLoanV4Address) ? "1" : "2";
+  const {
+    MultiSourceLoanV4Address,
+    MultiSourceLoanV5Address,
+    MultiSourceLoanV5_1Address,
+  } = getContracts({ id: chainId });
+
+  let version = "";
+
+  if (areSameAddress(verifyingContract, MultiSourceLoanV4Address)) {
+    version = "1";
+  } else if (areSameAddress(verifyingContract, MultiSourceLoanV5Address)) {
+    version = "2";
+  } else if (areSameAddress(verifyingContract, MultiSourceLoanV5_1Address)) {
+    version = "2"; // We deployed the new contract with the same version, but address differentiates it
+  } else {
+    throw new Error(`Invalid verifying contract address ${verifyingContract}`);
+  }
+
   return {
     name: "GONDI_MULTI_SOURCE_LOAN",
     version,
@@ -24,14 +40,20 @@ export const getDomain = (chainId: number, verifyingContract: Address) => {
 
 const toInteger = (bn: bigint | number): number => Number(bn.valueOf());
 
-export const millisToSeconds = (millis: number | bigint) => Math.ceil(toInteger(millis) / 1_000);
+export const millisToSeconds = (millis: number | bigint) =>
+  Math.ceil(toInteger(millis) / 1_000);
 export const SECONDS_IN_DAY = 60 * 60 * 24;
 
 export const bpsToPercentage = (bps: bigint | number) => toInteger(bps) / 10000;
 
-export const NATIVE_MARKETPLACE = 'MarketPlace.Native';
+export const NATIVE_MARKETPLACE = "MarketPlace.Native";
 
-export const emitLoanArgsToMslArgs = ({ offer, tokenId, amount, expirationTime }: {
+export const emitLoanArgsToMslArgs = ({
+  offer,
+  tokenId,
+  amount,
+  expirationTime,
+}: {
   offer: model.SingleNftOffer | model.CollectionOffer;
   tokenId: bigint;
   amount?: bigint;
