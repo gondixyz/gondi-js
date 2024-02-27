18ce77b983029ac2c0943e5860d003d2e39a038b
mishuagopian <mishuagopian@gmail.com>
2024-02-27 11:22:04 -0300

    fix: emit loan with collection offer working

--- examples/emitCollectionOfferLoan.ts
+++ examples/emitCollectionOfferLoan.ts
@@ -8,21 +8,26 @@ const emitAndRepayLoan = async (contract?: Address) => {
   console.log(`offer placed successfully: ${contractVersionString}`);
 
   const emitLoan = await users[1].emitLoan({
-    offer: {
-      ...signedOffer,
-      nftId: Number(signedOffer.nftCollateralTokenId.valueOf()),
-    },
+    offerExecution: [
+      {
+        offer: {
+          ...signedOffer,
+          nftId: Number(signedOffer.nftCollateralTokenId.valueOf()),
+          maxTrancheFloor: signedOffer.maxTrancheFloor ?? 0n,
+        },
+        lenderOfferSignature: signedOffer.signature,
+      },
+    ],
+    duration: signedOffer.duration,
     tokenId: testTokenId,
   });
-  const { loan } = await emitLoan.waitTxInBlock();
+
+  const { loan, loanId } = await emitLoan.waitTxInBlock();
   console.log(`loan emitted: ${contractVersionString}`);
 
   await sleep(3000);
 
-  const repayLoan = await users[1].repayLoan({
-    loan,
-    loanId: loan.source[0].loanId,
-  });
+  const repayLoan = await users[1].repayLoan({ loan, loanId });
   await repayLoan.waitTxInBlock();
   console.log(`loan repaid: ${contractVersionString}`);
 };
@@ -31,10 +36,14 @@ async function main() {
   try {
     await emitAndRepayLoan();
 
-    const MULTI_SOURCE_LOAN_CONTRACT_V4 = process.env.MULTI_SOURCE_LOAN_CONTRACT_V4 ?? '';
-
-    if (isAddress(MULTI_SOURCE_LOAN_CONTRACT_V4)) {
-      await emitAndRepayLoan(MULTI_SOURCE_LOAN_CONTRACT_V4);
+    const oldContracts = [
+      process.env.MULTI_SOURCE_LOAN_CONTRACT_V5 ?? '',
+      process.env.MULTI_SOURCE_LOAN_CONTRACT_V4 ?? '',
+    ];
+    for (const contract of oldContracts) {
+      if (isAddress(contract)) {
+        await emitAndRepayLoan(contract);
+      }
     }
   } catch (e) {
     console.log('Error:');
