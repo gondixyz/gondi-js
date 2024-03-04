3074a1775ae936c372d7682f625da6c0b1038e12
mishuagopian <mishuagopian@gmail.com>
2024-03-04 11:25:45 -0300

    fix: building types for renegotiation related methods

--- src/gondi.ts
+++ src/gondi.ts
@@ -21,13 +21,13 @@ import {
   zeroHash,
   zeroHex,
 } from '@/blockchain';
+import { getCurrencies } from '@/deploys';
 import { MarketplaceEnum, OffersSortField, Ordering } from '@/generated/graphql';
 import * as model from '@/model';
+import { Reservoir } from '@/reservoir/Reservoir';
+import { isNative, SeaportOrder } from '@/reservoir/utils';
 import { emitLoanArgsToMslArgs, NATIVE_MARKETPLACE } from '@/utils';
-
-import { getCurrencies } from './deploys';
-import { Reservoir } from './reservoir/Reservoir';
-import { isNative, SeaportOrder } from './reservoir/utils';
+import { loanToMslLoan, renegotiationToMslRenegotiation } from '@/utils/loan';
 
 type GondiProps = {
   wallet: Wallet;
@@ -247,6 +247,8 @@ export class Gondi {
       lenderAddress: this.wallet.account?.address,
       signerAddress: this.wallet.account?.address,
       ...renegotiation,
+      targetPrincipal: renegotiation.targetPrincipal ?? [],
+      trancheIndex: renegotiation.trancheIndex ?? [],
     };
     const response = await this.api.generateRenegotiationOfferHash({
       renegotiationInput,
@@ -491,18 +493,9 @@ export class Gondi {
     loan: LoanV4V5;
     loanId: bigint;
   }) {
-    const offerInput = {
-      ...offer,
-      loanId,
-      strictImprovement: offer.strictImprovement ?? false,
-      lender: offer.lenderAddress,
-      signer: offer.signerAddress ?? zeroAddress,
-      fee: offer.feeAmount,
-    };
-
     return this.contracts.Msl(loan.contractAddress).refinanceFullLoan({
-      offer: offerInput,
-      loan,
+      offer: renegotiationToMslRenegotiation(offer, loanId),
+      loan: loanToMslLoan(loan),
       signature: offer.signature,
     });
   }
@@ -516,18 +509,9 @@ export class Gondi {
     loan: LoanV4V5;
     loanId: bigint;
   }) {
-    const offerInput = {
-      ...offer,
-      loanId,
-      strictImprovement: offer.strictImprovement ?? false,
-      lender: offer.lenderAddress,
-      signer: offer.signerAddress ?? zeroAddress,
-      fee: offer.feeAmount,
-    };
-
     return this.contracts.Msl(loan.contractAddress).refinancePartialLoan({
-      offer: offerInput,
-      loan,
+      offer: renegotiationToMslRenegotiation(offer, loanId),
+      loan: loanToMslLoan(loan),
     });
   }
 
