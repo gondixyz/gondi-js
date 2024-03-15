6c88ad24dd8d83a893d6b68b7bfd4349b9c7293e
mishuagopian <mishuagopian@gmail.com>
2024-03-15 18:29:07 -0300

    chore: update dev v6 contracts

--- src/deploys.ts
+++ src/deploys.ts
@@ -53,7 +53,7 @@ export const getContracts = (chain: Pick<Chain, 'id'>): Contracts => {
           '0x610178dA211FEF7D417bC0e6FeD39F05609AD788',
         v6:
           ensureAddress(process.env.GONDI_MULTI_SOURCE_LOAN_V6) ??
-          '0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1',
+          '0x59b670e9fA9D0A427751Af201D676719a970857b',
       },
       AuctionLoanLiquidator: {
         v4:
@@ -64,7 +64,7 @@ export const getContracts = (chain: Pick<Chain, 'id'>): Contracts => {
           '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318',
         v6:
           ensureAddress(process.env.GONDI_AUCTION_LOAN_LIQUIDATOR_V6) ??
-          '0x59b670e9fA9D0A427751Af201D676719a970857b',
+          '0xc6e7DF5E7b4f2A278906862b61205850344D4e7d',
       },
       UserVault:
         ensureAddress(process.env.GONDI_USER_VAULT_V5) ??
