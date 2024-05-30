6952350eed59bd54a256dfa1e406852b9fc18c3d
Uziel Ludueña <uziel@floridastreet.xyz>
2024-05-30 10:02:56 -0300

    feat: withdraw many from queue

--- src/contracts/WithdrawalQueue.ts
+++ src/contracts/WithdrawalQueue.ts
@@ -1,4 +1,4 @@
-import { Address } from 'viem';
+import { Address, encodeFunctionData } from 'viem';
 
 import { filterLogs } from '@/blockchain';
 import { Wallet } from '@/contracts';
@@ -36,6 +36,37 @@ export class WithdrawalQueue extends BaseContract<typeof withdrawalQueueABI> {
         const events = filterLogs(receipt, filter);
         if (events.length === 0) throw new Error('Withdraw did not go through');
         return { ...events[0].args, ...receipt };
+      },
+    };
+  }
+
+  async withdrawMany({ to, tokenIds }: { to: Address; tokenIds: bigint[] }) {
+    if (tokenIds.length === 1) {
+      // This makes the tx readable in wallets
+      return this.withdraw({ to, tokenId: tokenIds[0] });
+    }
+
+    const txHash = await this.safeContractWrite.multicall([
+      tokenIds.map((tokenId) =>
+        encodeFunctionData({
+          abi: withdrawalQueueABI,
+          functionName: 'withdraw',
+          args: [to, tokenId],
+        }),
+      ),
+    ]);
+
+    return {
+      txHash,
+      waitTxInBlock: async () => {
+        const receipt = await this.bcClient.waitForTransactionReceipt({
+          hash: txHash,
+        });
+
+        const filter = await this.contract.createEventFilter.Withdrawn();
+        const events = filterLogs(receipt, filter);
+        if (events.length !== tokenIds.length) throw new Error('Withdraw did not go through');
+        return { ...receipt, results: events.map(({ args }) => args) };
       },
     };
   }
