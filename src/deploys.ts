c467e6537f621cef79ea50f9daaa37975c55196f
Mishu Agopian <mishuagopian@gmail.com>
2024-06-05 14:05:16 -0400

    chore: update anvil addresses (#78)

--- src/deploys.ts
+++ src/deploys.ts
@@ -56,7 +56,7 @@ export const getContracts = (chain: Pick<Chain, 'id'>): Contracts => {
           '0x610178dA211FEF7D417bC0e6FeD39F05609AD788',
         v6:
           ensureAddress(process.env.GONDI_MULTI_SOURCE_LOAN_V6) ??
-          '0x59b670e9fA9D0A427751Af201D676719a970857b',
+          '0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1',
       },
       AuctionLoanLiquidator: {
         v4:
@@ -67,7 +67,7 @@ export const getContracts = (chain: Pick<Chain, 'id'>): Contracts => {
           '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318',
         v6:
           ensureAddress(process.env.GONDI_AUCTION_LOAN_LIQUIDATOR_V6) ??
-          '0xc6e7DF5E7b4f2A278906862b61205850344D4e7d',
+          '0x59b670e9fA9D0A427751Af201D676719a970857b',
       },
       UserVault: {
         v5:
