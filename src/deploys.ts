8ee5ed320191d1745b1dbb14c66aa600e57cd90b
mishuagopian <mishuagopian@gmail.com>
2025-02-06 11:42:40 -0300

    fix: v6 addresses

--- src/deploys.ts
+++ src/deploys.ts
@@ -78,8 +78,12 @@ export const getContracts = (chain: Pick<Chain, 'id'>): Contracts => {
           '0x4A679253410272dd5232B3Ff7cF5dbB88f295319',
       },
       PurchaseBundler: {
-        v5: '0xTODO',
-        v6: '0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f',
+        v5:
+          ensureAddress(process.env.GONDI_PURCHASE_BUNDLER_V5) ??
+          '0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E',
+        v6:
+          ensureAddress(process.env.GONDI_PURCHASE_BUNDLER_V6) ??
+          '0x7a2088a1bFc9d81c55368AE168C2C02570cB814F',
       },
     };
   }
